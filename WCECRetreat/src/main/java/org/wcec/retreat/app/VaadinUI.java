a57ce6127cf12f1e85ad025da1d0d07a51406e30
chout <chout@192.168.1.8>
2018-01-02 19:49:41 -0500

    Prototyped with Grid concept - I have a TabSheet from the UI design called TableMaintenaceDesign.html.
    The code in VaadinUI.java will traverse to that TabSheet and add a new
    VerticalLayout. Then I added the newly created Grid control in it.

--- WCECRetreat/src/main/java/org/wcec/retreat/app/VaadinUI.java
+++ WCECRetreat/src/main/java/org/wcec/retreat/app/VaadinUI.java
@@ -1,30 +1,137 @@
 package org.wcec.retreat.app;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.util.StringUtils;
 import org.wcec.retreat.entity.EventTbl;
-import org.wcec.retreat.extendedgui.MyUpsertEventDesign;
+import org.wcec.retreat.extendedgui.MyTableMaintenanceDesign;
+import org.wcec.retreat.repo.AccessLevelRepo;
+import org.wcec.retreat.repo.AddressTblRepo;
+import org.wcec.retreat.repo.BedTypeTblRepo;
+import org.wcec.retreat.repo.BuildingTblRepo;
+import org.wcec.retreat.repo.ChurchTblRepo;
+import org.wcec.retreat.repo.EmailTblRepo;
+import org.wcec.retreat.repo.EventHostTblRepo;
 import org.wcec.retreat.repo.EventTblRepo;
+import org.wcec.retreat.repo.EventTypeTblRepo;
+import org.wcec.retreat.repo.GroupTblRepo;
+import org.wcec.retreat.repo.GroupTypeTblRepo;
+import org.wcec.retreat.repo.LodgingAssignmentTblRepo;
+import org.wcec.retreat.repo.MealPlanTblRepo;
+import org.wcec.retreat.repo.MealTblRepo;
+import org.wcec.retreat.repo.MealTblRepository;
+import org.wcec.retreat.repo.PaymentTblRepo;
+import org.wcec.retreat.repo.PersonTblRepository;
+import org.wcec.retreat.repo.PhoneTblRepo;
+import org.wcec.retreat.repo.PhoneTypeTblRepo;
+import org.wcec.retreat.repo.RegistrationTblRepo;
+import org.wcec.retreat.repo.RoomTblRepo;
+import org.wcec.retreat.repo.UserLoginRepo;
+import org.wcec.retreat.repo.UserTblRepo;
 
+import com.google.gwt.thirdparty.guava.common.collect.Lists;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @SpringUI
 public class VaadinUI extends UI {
+	@Autowired
+	AccessLevelRepo                            TheAccessLevelRepo;
 	
+	@Autowired
+	AddressTblRepo                             TheAddressTblRepo;
+	
+	@Autowired
+	BedTypeTblRepo                             TheBedTypeTblRepo;
+	
+	@Autowired
+	BuildingTblRepo                            TheBuildingTblRepo;
+	
+	@Autowired
+	ChurchTblRepo                              TheChurchTblRepo;
+	
+	@Autowired
+	EmailTblRepo                               TheEmailTblRepo;
+	
+	@Autowired
+	EventHostTblRepo                           TheEventHostTblRepo;
+	
+	@Autowired
+	EventTblRepo                               TheEventTblRepo;
+	
+	@Autowired
+	EventTypeTblRepo                           TheEventTypeTblRepo;
+	
+	@Autowired
+	GroupTblRepo                               TheGroupTblRepo;
+	
+	@Autowired
+	GroupTypeTblRepo                           TheGroupTypeTblRepo;
+	
+	@Autowired
+	LodgingAssignmentTblRepo                   TheLodgingAssignmentTblRepo;
+	
+	@Autowired
+	MealPlanTblRepo                            TheMealPlanTblRepo;
+	
+	@Autowired
+	MealTblRepo                                TheMealTblRepo;
+	
+	@Autowired
+	MealTblRepository                          TheMealTblRepository;
+	
+	@Autowired
+	PaymentTblRepo                             ThePaymentTblRepo;
+	
+	@Autowired
+	PersonTblRepository                        ThePersonTblRepository;
+	
+	@Autowired
+	PhoneTblRepo                               ThePhoneTblRepo;
+	
+	@Autowired
+	PhoneTypeTblRepo                           ThePhoneTypeTblRepo;
+	
+	@Autowired
+	RegistrationTblRepo                        TheRegistrationTblRepo;
+	
+	@Autowired
+	RoomTblRepo                                TheRoomTblRepo;
+	
+	@Autowired
+	UserLoginRepo                              TheUserLoginRepo;
+	
+	@Autowired
+	UserTblRepo                                TheUserTblRepo;
+	
+	SimpleJpaRepository                        TheRepository;
+
+	 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private final EventTblRepo eventTblRepo;
 
 	private final EventTblEditor editor;
 
-	final Grid<EventTbl> grid;
+	 
 
 	final TextField filter;
 
@@ -35,23 +142,73 @@ public class VaadinUI extends UI {
 	public VaadinUI(EventTblRepo eventTblRepo, EventTblEditor editor) {
 		this.eventTblRepo = eventTblRepo;
 		this.editor = editor;
-		this.grid = new Grid<>(EventTbl.class);
+		
+		
 		this.filter = new TextField();
 		this.addNewBtn = new Button("New meal type", FontAwesome.PLUS);
 	}
-
+	
+	
+	 
 	 
 
+	void findTabSheet(ComponentContainer aComponentContainer, List<TabSheet> outList) {
+		Iterator<Component> itr = aComponentContainer.getComponentIterator();
+		while (itr.hasNext()) {
+			Component comp = itr.next();
+			System.out.println("Comp = " + comp);
+			if (comp instanceof TabSheet) {
+				outList.add((TabSheet)comp);
+			}
+			if (comp instanceof ComponentContainer) {
+				findTabSheet((ComponentContainer) comp, outList);
+			}
+		}
+	}
+	
 	@Override
 	protected void init(VaadinRequest request) {
 		// build layout
 		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
 		//VerticalLayout mainLayout = new VerticalLayout(actions, grid, editor);
-		VerticalLayout mainLayout = new MyUpsertEventDesign(); 
+		VerticalLayout mainLayout = new MyTableMaintenanceDesign(); 
+		ArrayList<TabSheet> outList = new ArrayList<TabSheet>();
+		findTabSheet((ComponentContainer) mainLayout, outList); 
 		setContent(mainLayout);
-/*
-		grid.setHeight(300, Unit.PIXELS);
-		grid.setColumns("id", "mealType", "ageFrom", "ageTo", "mealPrice");
+		TabSheet sheet = outList.get(0);
+		Grid<EventTbl> theGrid = new Grid<>(EventTbl.class);
+		
+		List<Field> currentClassFields = (List) Lists.newArrayList(EventTbl.class.getDeclaredFields());
+		String fieldNames[] = new String[currentClassFields.size()];
+		int actualSize = 0;
+		for (int i = 0, j = 0; i < fieldNames.length; i++) {
+			if (!currentClassFields.get(i).getName().equals("serialVersionUID")) {
+				fieldNames[j] = currentClassFields.get(i).getName();
+				j++;
+				actualSize = j;
+			}
+		}
+		String fieldNames2[] = new String[actualSize];
+		for (int i = 0, j = 0; i < fieldNames.length; i++) {
+			if (fieldNames[i] != null) { 
+				fieldNames2[j] = fieldNames[i];
+				j++;
+			}
+		}
+		
+		VerticalLayout tab1 = new VerticalLayout(); 
+		sheet.addTab(tab1, "Domain Table", null); 
+		tab1.setVisible(true);
+		
+		
+		tab1.addComponent(theGrid);
+		theGrid.setHeight(100, Unit.PERCENTAGE);
+		theGrid.setWidth(100, Unit.PERCENTAGE);
+		theGrid.setVisible(true);
+		theGrid.setColumns(fieldNames2);
+		theGrid.setItems(eventTblRepo.findAll());
+		
+		/*
 
 		filter.setPlaceholder("Filter by meal type");
 
@@ -74,16 +231,16 @@ public class VaadinUI extends UI {
 			editor.setVisible(false);
 			listEventTbls(filter.getValue());
 		});
-
+*/
 		// Initialize listing
 		listEventTbls(null);
-		*/
+		 
 	}
 
 	// tag::listEventTbls[]
 	void listEventTbls(String filterText) {
 		if (StringUtils.isEmpty(filterText)) {
-			grid.setItems(eventTblRepo.findAll());
+			//grid.setItems(eventTblRepo.findAll());
 		} 
 	}
 	// end::listEventTbls[]
